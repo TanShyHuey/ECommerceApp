c689535ef0e1e4e689f8e5258866476b635eb717
XiaoTong <wongxt-wm19@student.tarc.edu.my>
2020-08-19 19:44:42 +0800

    I am dabest:)))

--- app/src/main/java/com/example/ecommerceapp/UserProfile.java
+++ app/src/main/java/com/example/ecommerceapp/UserProfile.java
@@ -1,32 +1,55 @@
 package com.example.ecommerceapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 public class UserProfile extends AppCompatActivity {
 
-    EditText fullName, email, phoneNo, password;
-    TextView fullNameLabel, usernameLabel;
+    private EditText fullName, email, phoneNo, password;
+    private TextView fullNameLabel, usernameLabel;
 
-    String _USERNAME, _NAME, _EMAIL, _PHONENO, _PASSWORD;
-    DatabaseReference reference;
+    private String _USERNAME, _NAME, _EMAIL, _PHONENO, _PASSWORD;
+    private DatabaseReference reference;
+    private StorageReference storageReference;
+    private FirebaseStorage storage;
+    private Uri imageUri;
+    private StorageTask uploadTask;
+    private ImageView profilePic;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-
-        reference = FirebaseDatabase.getInstance().getReference("users");
+        profilePic=findViewById(R.id.profilePic);
+        storage=FirebaseStorage.getInstance();
+        storageReference=FirebaseStorage.getInstance().getReference("uploads");
+        reference = FirebaseDatabase.getInstance().getReference("Users");
 
         fullName = findViewById(R.id.full_name_profile);
         email = findViewById(R.id.email_profile);
@@ -36,6 +59,61 @@ public class UserProfile extends AppCompatActivity {
         usernameLabel = findViewById(R.id.username_field);
 
         showAllUserData();
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choosePicture();
+            }
+        });
+    }
+
+    private void choosePicture() {
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,@Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
+            imageUri=data.getData();
+            profilePic.setImageURI(imageUri);
+            uploadPicture();
+        }
+    }
+
+    private void uploadPicture() {
+        final ProgressDialog pd=new ProgressDialog(this);
+        pd.setTitle("Uploading Image...");
+        pd.show();
+        final String randomKey= UUID.randomUUID().toString();
+        StorageReference riversRef=storageReference.child("images/"+randomKey);
+        riversRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        pd.dismiss();
+                        Snackbar.make(findViewById(android.R.id.content),"Image Uploaded.",Snackbar.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        pd.dismiss();
+                        Toast.makeText(getApplicationContext(),"Failed To Upload",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>(){
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        double progressPercent=(100.00*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                        pd.setMessage("Percentage:"+(int)progressPercent+"%");
+                    }
+                });
     }
 
     private void showAllUserData() {
