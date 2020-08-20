8c824167d6e7d9ce74a22d9bf29612cba1f81514
user <cheokjl-wm18@student.tarc.edu.my>
2020-08-20 18:21:21 +0800

    commit by junlin

---
+++ app/src/main/java/com/example/ecommerceapp/product.java
@@ -0,0 +1,62 @@
+package com.example.ecommerceapp;
+
+public class product {
+    private String product_name_details;
+    private Integer myButton;
+    private Integer spinner1;
+    private Integer spinner2;
+    private Float product_image_price;
+    private String txtSize;
+
+    public String getProduct_name_details() {
+        return product_name_details;
+    }
+
+    public void setProduct_name_details(String product_name_details) {
+        this.product_name_details = product_name_details;
+    }
+
+    public Integer getMyButton() {
+        return myButton;
+    }
+
+    public void setMyButton(Integer myButton) {
+        this.myButton = myButton;
+    }
+
+    public Integer getSpinner1() {
+        return spinner1;
+    }
+
+    public void setSpinner1(Integer spinner1) {
+        this.spinner1 = spinner1;
+    }
+
+    public Integer getSpinner2() {
+        return spinner2;
+    }
+
+    public void setSpinner2(Integer spinner2) {
+        this.spinner2 = spinner2;
+    }
+
+    public Float getProduct_image_price() {
+        return product_image_price;
+    }
+
+    public void setProduct_image_price(Float product_image_price) {
+        this.product_image_price = product_image_price;
+    }
+
+    public String getTxtSize() {
+        return txtSize;
+    }
+
+    public void setTxtSize(String txtSize) {
+        this.txtSize = txtSize;
+    }
+
+    public product() {
+
+    }
+}
