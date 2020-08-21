1bd9c2ce50c1c7248d2b5f26b98bf00106c9badf
Leong Sam Kuen <Leongsk-wm19@student.tarc.edu.my>
2020-08-21 20:41:12 +0800

    SK

---
+++ app/src/main/java/com/example/ecommerceapp/CartList.java
@@ -0,0 +1,41 @@
+package com.example.ecommerceapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class CartList extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private Button NextProcessBtn;
+    private TextView txtTotalAmount;
+    private ViewGroup Parent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart_list);
+
+        recyclerView = findViewById(R.id.cart_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        NextProcessBtn = (Button) findViewById(R.id.next_process_btn);
+        txtTotalAmount = (TextView) findViewById(R.id.total_price);
+    }
+
+}
