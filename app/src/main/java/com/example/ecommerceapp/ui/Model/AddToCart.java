7c1daee2899753094b1ac83ed73074616cf2a34a
Leong Sam Kuen <Leongsk-wm19@student.tarc.edu.my>
2020-08-27 00:08:24 +0800

    SK

--- app/src/main/java/com/example/ecommerceapp/ui/Model/AddToCart.java
+++ app/src/main/java/com/example/ecommerceapp/ui/Model/AddToCart.java
@@ -3,8 +3,12 @@ package com.example.ecommerceapp.ui.Model;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 
 import android.os.Bundle;
+
+import com.example.ecommerceapp.R;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class AddToCart extends AppCompatActivity
@@ -19,10 +23,10 @@ public class AddToCart extends AppCompatActivity
         recview=(RecyclerView)findViewById(R.id.recview);
         recview.setLayoutManager(new LinearLayoutManager(this));
 
-        FirebaseRecyclerOption<Chat> option =
-                new FirebaseRecyclerOption.Builder<Chat>()
-                        .setQuery(query, Chat.class)
-                        .build;
+        FirebaseRecyclerOptions<model> option =
+                new FirebaseRecyclerOptions.Builder<model>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("Product"), model.class)
+                        .build();
 
     }
 }
