ad66e2aeac380f17f2dfd648e1b98da1d5f2784b
Leong Sam Kuen <Leongsk-wm19@student.tarc.edu.my>
2020-09-05 10:18:41 +0800

    SK

--- app/src/main/java/com/example/ecommerceapp/Comestic4.java
+++ app/src/main/java/com/example/ecommerceapp/Comestic4.java
@@ -111,14 +111,14 @@ public class Comestic4 extends AppCompatActivity implements AdapterView.OnItemSe
         FavproductName=(TextView) findViewById(R.id.ProductName);
         FavproductPrice=(TextView) findViewById(R.id.ProductPrice);
         Favbtn = (ElegantNumberButton) findViewById(R.id.Quantity);
-        FavProductSize=(Spinner) findViewById(R.id.Size);
+//        FavProductSize=(Spinner) findViewById(R.id.Size);
         FavShippingList=(Spinner)findViewById(R.id.Shipping);
         btnFavouriteProduct=(Button) findViewById(R.id.Favourite_button);
 
         btnFavouriteProduct.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                favourite.setFavSize(FavProductSize.getSelectedItem().toString());
+//                favourite.setFavSize(FavProductSize.getSelectedItem().toString());
                 favourite.setFavProductName(FavproductName.getText().toString().trim());
                 favourite.setFavProductPrice(FavproductPrice.getText().toString().trim());
                 favourite.setFavShipping(FavShippingList.getSelectedItem().toString());
