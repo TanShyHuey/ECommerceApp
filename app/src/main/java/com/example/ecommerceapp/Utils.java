280a0216ad7588ba757337fffcc9ecd85625f0ff
WongXiaoTong <66934914+WongXiaoTong@users.noreply.github.com>
2020-09-19 09:38:39 +0800

    Update Utils.java

--- app/src/main/java/com/example/ecommerceapp/Utils.java
+++ app/src/main/java/com/example/ecommerceapp/Utils.java
@@ -3,6 +3,6 @@ package com.example.ecommerceapp;
 public class Utils {
     // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
 
-    public static final  String EMAIL = "wongxt-wm19@student.tarc.edu.my";
-    public static final String PASSWORD = "010730140996";
+    public static final  String EMAIL = "";
+    public static final String PASSWORD = "";
 }
